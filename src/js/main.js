a1648e85b9d2f4d4b652df9202282a719a88a1e3
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-08 00:29:23 +0200

    Add JS event - rotate button

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const buttonArrowDesign = document.querySelector(".form__box__button--design");
+const buttonArrowFill = document.querySelector(".form__box__button--fill");
+const buttonArrowShare = document.querySelector(".form__box__button--share");
+
+function handleButtonArrowClick(event) {
+  const openedButton = event.currentTarget;
+  openedButton.classList.toggle("btn-rotate");
+  console.log("rotate");
+
+  //   const formBoxDesign = document.querySelector(".form__color");
+  //   const formBoxFill = document.querySelector(".form__data");
+  //   const formBoxShare = document.querySelector(".form__share--button");
+
+  //   const showContent = event.currentTarget;
+  //   showContent.classList.toggle("visible");
+  //   console.log("visible");
+}
+
+buttonArrowDesign.addEventListener("click", handleButtonArrowClick);
+buttonArrowFill.addEventListener("click", handleButtonArrowClick);
+buttonArrowShare.addEventListener("click", handleButtonArrowClick);
