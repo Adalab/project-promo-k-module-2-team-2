7b6f539c2fea2c51038ac44a368c20f31d68dac1
Susana <su.gonzalez.vejo@gmail.com>
2020-10-08 23:53:32 +0200

    Add reset

--- src/js/reset.js
+++ src/js/reset.js
@@ -3,12 +3,14 @@
 
 //el value de input en el elemento de nombre
 const inputName = document.querySelector('.js-input-name');
+const inputJob = document.querySelector ('.js-input-job');
 const dataName = inputName.value;
 const name = document.querySelector ('.js-name');
 name.innerHTML = dataName;
 
 
 //crear variables con los elementos del listener y los que queremos cambiar en la tarjeta,
+const input = document.querySelector('.js-input-name');
 const reset = document.querySelector ('.js-reset');
 const job = document.querySelector ('.js-job');
 
@@ -17,18 +19,24 @@ const resetButton1 = {};
 resetButton1.name = 'Nombre Apellido';
 resetButton1.job = 'Front-end developer';
 
+//funcion para que aparezca el value en el name
+function changeInput (){
+  return name.innerHTML = inputName;
+}
+
 
 // //funcion a cada propiedad
 function changeName (){
   return name.innerHTML =  resetButton1.name;
-
 }
 
+
+
 // function changeJob (){
-//   resetButton1.job = 'STUDENT';
 //   return job.innerHTML = resetButton1.job;
 // }
 
 // //evento sobre el botón.
+// name.addEventListener('keyup',changeInput);
 reset.addEventListener ('click',changeName);
 // reset.addEventListener ('click',changeJob);
