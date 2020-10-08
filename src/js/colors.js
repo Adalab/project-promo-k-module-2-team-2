3d33cf0756d56a2efc9c22b3499fcc7b7aef0445
Yanira <yanirafr@gmail.com>
2020-10-08 20:53:31 +0200

    add js design color border and circle

--- src/js/colors.js
+++ src/js/colors.js
@@ -6,23 +6,55 @@ const colorOption3 = document.querySelector(".js-color3");
 
 function colorChange() {
   const name = document.querySelector(".js-name");
+  const border = document.querySelector(".js-border");
+  const circle = document.querySelector(".js-circ");
+
   name.classList.remove("txt-dark");
+  border.classList.remove("brd");
+  circle.classList.remove("circ");
+
   if (colorOption1.checked === true) {
-    name.classList.add("first-option");
-    name.classList.remove("second-option");
-    name.classList.remove("third-option");
+    name.classList.add("nm-op1");
+    name.classList.remove("nm-op2");
+    name.classList.remove("nm-op3");
+
+    border.classList.add("bd-op1");
+    border.classList.remove("bd-op2");
+    border.classList.remove("bd-op3");
+
+    circle.classList.add("cr-op1");
+    circle.classList.remove("cr-op2");
+    circle.classList.remove("cr-op3");
   }
   if (colorOption2.checked === true) {
-    name.classList.add("second-option");
-    name.classList.remove("first-option");
-    name.classList.remove("third-option");
+    name.classList.add("nm-op2");
+    name.classList.remove("nm-op1");
+    name.classList.remove("nm-op3");
+
+    border.classList.add("bd-op2");
+    border.classList.remove("bd-op1");
+    border.classList.remove("bd-op3");
+
+    circle.classList.add("cr-op2");
+    circle.classList.remove("cr-op1");
+    circle.classList.remove("cr-op3");
   }
   if (colorOption3.checked === true) {
-    name.classList.add("third-option");
-    name.classList.remove("first-option");
-    name.classList.remove("second-option");
+    name.classList.add("nm-op3");
+    name.classList.remove("nm-op1");
+    name.classList.remove("nm-op2");
+
+    border.classList.add("bd-op3");
+    border.classList.remove("bd-op1");
+    border.classList.remove("bd-op2");
+
+    circle.classList.add("cr-op3");
+    circle.classList.remove("cr-op1");
+    circle.classList.remove("cr-op2");
   }
   console.log(name);
+  console.log(border);
+  console.log(circle);
 }
 
 colorOption1.addEventListener("click", colorChange);
