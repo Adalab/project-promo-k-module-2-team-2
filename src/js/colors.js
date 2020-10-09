d32a6a6ee5b82691667ba7a8bfb7d24b6fac9b47
Yanira <yanirafr@gmail.com>
2020-10-09 12:48:15 +0200

    improve js code

--- src/js/colors.js
+++ src/js/colors.js
@@ -10,35 +10,17 @@ const colorOption3 = document.querySelector(".js-color3");
 
 function colorChange() {
 
-  // Card selectors
+  // Card selector
 
-  const name = document.querySelector(".js-name");
-  const border = document.querySelector(".js-border");
-  const circle = document.querySelector(".js-circ");
-  const icon = document.querySelector(".js-ico");
   const palette = document.querySelector(".js-palette");
 
-  // Card default class remove
+  // Card class remove
 
-  name.classList.remove("txt-dark");
-  border.classList.remove("brd");
-  circle.classList.remove("circ");
+  palette.classList.remove("default-palette");
 
   palette.classList.remove("palette1");
   palette.classList.remove("palette2");
   palette.classList.remove("palette3");
-  
-  name.classList.remove("nm-op2");
-  name.classList.remove("nm-op3");
-
-  border.classList.remove("bd-op2");
-  border.classList.remove("bd-op3");
-  
-  circle.classList.remove("cr-op2");
-  circle.classList.remove("cr-op3");
-  
-  icon.classList.remove("ico-op2");
-  icon.classList.remove("ico-op3");
 
   // Option 1
 
@@ -52,41 +34,19 @@ function colorChange() {
 
   if (colorOption2.checked === true) {
 
-    //Name 2
-    name.classList.add("nm-op2");
-
-    //Border 2
-    border.classList.add("bd-op2");
-
-    //Circle 2
-    circle.classList.add("cr-op2");
-
-    // Icon 2
-    icon.classList.add("ico-op2");
+    palette.classList.add("palette2");
   }
 
 // Option 3
 
   if (colorOption3.checked === true) {
 
-    //Name 3
-    name.classList.add("nm-op3");
+    palette.classList.add("palette3");
 
-    //Border 3
-    border.classList.add("bd-op3");
-
-    //Circle 3
-    circle.classList.add("cr-op3");
-
-    // Icon 3
-    icon.classList.add("ico-op3");
   }
 
   // Console log
-  console.log(name);
-  console.log(border);
-  console.log(circle);
-  console.log(icon);
+  console.log(palette);
 }
 
 // Event listeners on input
