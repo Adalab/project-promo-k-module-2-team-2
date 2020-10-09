5be15f401347fdbe56f588072a31ef7ef80f7dae
Yanira <yanirafr@gmail.com>
2020-10-09 12:59:57 +0200

    change js quotation

--- src/js/colors.js
+++ src/js/colors.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 // Color input selectors
 
-const colorOption1 = document.querySelector(".js-color1");
-const colorOption2 = document.querySelector(".js-color2");
-const colorOption3 = document.querySelector(".js-color3");
+const colorOption1 = document.querySelector('.js-color1');
+const colorOption2 = document.querySelector('.js-color2');
+const colorOption3 = document.querySelector('.js-color3');
 
 //Color change function
 
@@ -12,42 +12,42 @@ function colorChange() {
 
   // Card selector
 
-  const palette = document.querySelector(".js-palette");
+  const palette = document.querySelector('.js-palette');
 
   // Card class remove
 
-  palette.classList.remove("default-palette");
+  palette.classList.remove('default-palette');
 
-  palette.classList.remove("palette1");
-  palette.classList.remove("palette2");
-  palette.classList.remove("palette3");
+  palette.classList.remove('palette1');
+  palette.classList.remove('palette2');
+  palette.classList.remove('palette3');
 
   // Option 1
 
   if (colorOption1.checked === true) {
 
     //Palette 1
-    palette.classList.add("palette1");
+    palette.classList.add('palette1');
   }
 
   // Option 2
 
   if (colorOption2.checked === true) {
 
-    palette.classList.add("palette2");
+    palette.classList.add('palette2');
   }
 
   // Option 3
 
   if (colorOption3.checked === true) {
 
-    palette.classList.add("palette3");
+    palette.classList.add('palette3');
 
   }
 }
 
 // Event listeners on input
 
-colorOption1.addEventListener("click", colorChange);
-colorOption2.addEventListener("click", colorChange);
-colorOption3.addEventListener("click", colorChange);
+colorOption1.addEventListener('click', colorChange);
+colorOption2.addEventListener('click', colorChange);
+colorOption3.addEventListener('click', colorChange);
