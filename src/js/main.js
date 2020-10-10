10a19a5fe1bf26da694b318565b72d0d79aaa3b1
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-10 22:06:50 +0200

    Add changes in js buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 "use strict";
 
-const buttonArrowDesign = document.querySelector(".form__box__button--design");
-const buttonArrowFill = document.querySelector(".form__box__button--fill");
-const buttonArrowShare = document.querySelector(".form__box__button--share");
+const buttonArrowDesign = document.querySelector(
+  ".js-form__box__button--design"
+);
+const buttonArrowFill = document.querySelector(".js-form__box__button--fill");
+const buttonArrowShare = document.querySelector(".js-form__box__button--share");
 
-const formBoxDesign = document.querySelector(".form__color");
+const formBoxDesign = document.querySelector(".js-form__color");
 const formBoxFill = document.querySelector(".form__data");
 const formBoxShare = document.querySelector(".form__share--button");
 
@@ -13,10 +15,24 @@ function handleButtonArrowClick(event) {
   openedButton.classList.toggle("btn-rotate");
   console.log("rotate");
 
-  if (openedButton.classList.contains("btn-rotate") === true) {
+  if (buttonArrowDesign.classList.contains("btn-rotate") === true) {
+    formBoxDesign.classList.add("visible");
+    console.log("visible");
+  } else if (buttonArrowDesign.classList.contains("btn-rotate") === false) {
+    formBoxDesign.classList.remove("visible");
+  }
+
+  if (buttonArrowFill.classList.contains("btn-rotate") === true) {
+    formBoxFill.classList.add("visible");
+    console.log("visible");
+  } else if (buttonArrowFill.classList.contains("btn-rotate") === false) {
+    formBoxFill.classList.remove("visible");
+  }
+
+  if (buttonArrowShare.classList.contains("btn-rotate") === true) {
     formBoxShare.classList.add("visible");
     console.log("visible");
-  } else if (openedButton.classList.contains("btn-rotate") === false) {
+  } else if (buttonArrowShare.classList.contains("btn-rotate") === false) {
     formBoxShare.classList.remove("visible");
   }
 
@@ -26,3 +42,14 @@ function handleButtonArrowClick(event) {
 buttonArrowDesign.addEventListener("click", handleButtonArrowClick);
 buttonArrowFill.addEventListener("click", handleButtonArrowClick);
 buttonArrowShare.addEventListener("click", handleButtonArrowClick);
+
+const buttonSubmit = document.querySelector(".form__share--button");
+
+function handleButtonSubmit(event) {
+  buttonSubmit.classList.add("change-color");
+  console.log("change color");
+
+  event.preventDefault();
+}
+
+buttonSubmit.addEventListener("click", handleButtonSubmit());
