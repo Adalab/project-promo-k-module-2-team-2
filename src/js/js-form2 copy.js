8f1d77451a329168a93758c6d8159dd334529893
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-15 14:19:33 +0200

    Add opmtimized version

--- src/js/js-form2 copy.js
+++ src/js/js-form2 copy.js
@@ -41,70 +41,95 @@ const formData = {
   },
 };
 
+function fillCard(event) {
+  const input = event.currentTarget;
+  const inputId = input.id;
+  const inputValue = input.value;
+  const previewCardElement = document.querySelector(`.js-preview-${inputId}`);
+  const previewCardElementProperty = formData[inputId].property;
+  const previewCardElementPlaceholder = formData[inputId].placeholder;
+
+  if (inputValue === "") {
+    previewCardElement[
+      previewCardElementProperty
+    ] = previewCardElementPlaceholder;
+  } else {
+    previewCardElement[previewCardElementProperty] = inputValue;
+  }
+}
+
+for (let formDataElement in formData) {
+  const inputClass = document.querySelector(`.js-intro-${formDataElement}`);
+  inputClass.addEventListener("keyup", fillCard);
+}
+
+// const introName = document.querySelector(".js-intro-name");
+// introName.addEventListener("keyup", fillCard);
+
 //Name//
-const introName = document.querySelector(".js-intro-name");
-const prevName = document.querySelector(".js-preview-name");
+// const introName = document.querySelector(".js-intro-name");
+// const prevName = document.querySelector(".js-preview-name");
 
-function putName() {
-  prevName.innerHTML = introName.value;
-  if (introName.value === "") {
-    prevName.innerHTML = "Nombre Apellido";
-  }
-  if (introName.value.length > introName.maxLength)
-    introName.value = introName.value.slice(0, introName.maxLength);
-}
+// function putName() {
+//   prevName.innerHTML = introName.value;
+//   if (introName.value === "") {
+//     prevName.innerHTML = "Nombre Apellido";
+//   }
+//   if (introName.value.length > introName.maxLength)
+//     introName.value = introName.value.slice(0, introName.maxLength);
+// }
 
-introName.addEventListener("keyup", putName);
+// introName.addEventListener("keyup", putName);
 //Job//
-const introJob = document.querySelector(".js-intro-job");
-const prevJob = document.querySelector(".js-preview-job");
+// const introJob = document.querySelector(".js-intro-job");
+// const prevJob = document.querySelector(".js-preview-job");
 
-function putJob() {
-  prevJob.innerHTML = introJob.value;
-  if (introJob.value === "") {
-    prevJob.innerHTML = "Front-end developer";
-  }
-  // if (introJob.value.length>introJob.maxLength) introJob.value = introJob.value.slice (0, introJob.maxLength);
-}
+// function putJob() {
+//   prevJob.innerHTML = introJob.value;
+//   if (introJob.value === "") {
+//     prevJob.innerHTML = "Front-end developer";
+//   }
+//   // if (introJob.value.length>introJob.maxLength) introJob.value = introJob.value.slice (0, introJob.maxLength);
+// }
 
-introJob.addEventListener("keyup", putJob);
+// introJob.addEventListener("keyup", putJob);
 
-//Phone//
-const introPhone = document.querySelector(".js-intro-phone");
-const prevPhone = document.querySelector(".js-preview-phone");
+// //Phone//
+// const introPhone = document.querySelector(".js-intro-phone");
+// const prevPhone = document.querySelector(".js-preview-phone");
 
-function putPhone() {
-  prevPhone.href = "tel:" + introPhone.value;
-}
+// function putPhone() {
+//   prevPhone.href = "tel:" + introPhone.value;
+// }
 
-introPhone.addEventListener("keyup", putPhone);
+// introPhone.addEventListener("keyup", putPhone);
 
-//Mail//
-const intMail = document.querySelector(".js-intro-mail");
-const preMail = document.querySelector(".js-preview-mail");
+// //Mail//
+// const intMail = document.querySelector(".js-intro-mail");
+// const preMail = document.querySelector(".js-preview-mail");
 
-function puMail() {
-  preMail.href = "mailto:" + intMail.value;
-}
+// function puMail() {
+//   preMail.href = "mailto:" + intMail.value;
+// }
 
-intMail.addEventListener("keyup", puMail);
+// intMail.addEventListener("keyup", puMail);
 
-//Linkedin//
-const introLinkedin = document.querySelector(".js-intro-linkedin");
-const prevLinkedin = document.querySelector(".js-preview-linkedin");
+// //Linkedin//
+// const introLinkedin = document.querySelector(".js-intro-linkedin");
+// const prevLinkedin = document.querySelector(".js-preview-linkedin");
 
-function putLinkedin() {
-  prevLinkedin.href = introLinkedin.value;
-}
+// function putLinkedin() {
+//   prevLinkedin.href = introLinkedin.value;
+// }
 
-introLinkedin.addEventListener("keyup", putLinkedin);
+// introLinkedin.addEventListener("keyup", putLinkedin);
 
-//Github//
-const introGithub = document.querySelector(".js-intro-github");
-const prevGithub = document.querySelector(".js-preview-github");
+// //Github//
+// const introGithub = document.querySelector(".js-intro-github");
+// const prevGithub = document.querySelector(".js-preview-github");
 
-function putGithub() {
-  prevGithub.href = "https://github.com/" + introGithub.value;
-}
+// function putGithub() {
+//   prevGithub.href = "https://github.com/" + introGithub.value;
+// }
 
-introGithub.addEventListener("keyup", putGithub);
+// introGithub.addEventListener("keyup", putGithub);
