872eda10357450a7d6c7119452ab5cd826cf2074
mteresa-jimenez <mteresa.jn@gmail.com>
2020-10-21 17:46:55 +0200

    Clean folders

--- src/js/twitter.js
+++
@@ -1,9 +0,0 @@
-// function putData(){
-// fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/");
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     document.body.innerHTML = data.result;
-//   });
-// }
