6a7c6e612ff878e54c2c0b0219bf70d1db46b05e
Yanira <yanirafr@gmail.com>
2020-10-27 12:09:40 +0100

    add twitter hashtags and via to constants

--- src/js/share-link.js
+++ src/js/share-link.js
@@ -26,11 +26,13 @@ function showURL(result) {
   const twitterText = encodeURIComponent(
     "¡He creado mi tarjeta con Catrina's profile cards!"
   );
+  const twitterHashtag = "Catrinas,Team2";
+  const twitterVia = "&via=yanira_fr,@arantxa";
 
   if (result.success === true) {
     cardLink.innerHTML = result.cardURL;
     cardLink.href = result.cardURL;
-    buttonTwitter.href = `https://twitter.com/intent/tweet?text=${twitterText}&url=${result.cardURL}&hashtags=Catrinas,Team2&via=yanira_fr,@arantxa`;
+    buttonTwitter.href = `https://twitter.com/intent/tweet?text=${twitterText}&url=${result.cardURL}&hashtags=${twitterHashtag}${twitterVia}`;
     buttonTwitter.innerHTML = `<i class="fab fa-twitter"></i> Compartir en twitter</a>`;
   } else if (result.error === undefined) {
     cardLink.innerHTML = `Creando tu link</br><i class="fas fa-spinner"></i>`;
